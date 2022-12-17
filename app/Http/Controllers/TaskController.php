683a815b34c99773ce50eb4d7085841605be4f20
sakata007 <be_best72@yahoo.co.jp>
2022-12-17 16:52:00 +0900

    no4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,24 +9,17 @@ use App\Models\Task;
 class TaskController extends Controller
 {
     /**
-     * Display a listing of the resource.
      *
+     *タスク一覧
      * @return \Illuminate\Http\Response
      */
+
     public function index()
     {
-        //
+        return Task::orderByDesc('id')->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -50,16 +43,7 @@ class TaskController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
